324774fd6ba1404d9ab6632287a9f040066dd434
ho8ae <xogh2242@gmail.com>
2025-05-19 18:08:10 +0900

    fix: not need Image import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useLanguage } from './context/LanguageContext';
 import Layout from './components/Layout';
-import Image from 'next/image';
 import Link from 'next/link';
 import ClientLayout from './client-layout';
 import { useEffect } from 'react';
